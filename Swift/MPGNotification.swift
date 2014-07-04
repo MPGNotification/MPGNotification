4f5aba1c7383a586d2b44f76110a220662cb347a
Gaurav Wadhwani <gaurav@mappgic.com>
2014-07-04 15:57:02 +0530

    - Added swift implementation of control and a basic demo project

---
+++ Swift/MPGNotification.swift
@@ -0,0 +1,27 @@
+//
+//  MPGNotification.swift
+//  MPGNotificationDemo
+//
+//  Created by Gaurav Wadhwani on 02/07/14.
+//  Copyright (c) 2014 Mappgic. All rights reserved.
+//
+
+import UIKit
+
+class MPGNotification: UIView {
+
+    init(frame: CGRect) {
+        super.init(frame: frame)
+        // Initialization code
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
